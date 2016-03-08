d71dce3c2d5ceb42f5944b4eb480be2755673148
Rares Tanase <r.tanase@rosoftlab.net>
2016-03-08 15:18:19 +0200

    Fixed code to open VanillaNAV app or iTunes Store, added plugin dependency to check platform

--- www/vn.js
+++ www/vn.js
@@ -6,21 +6,17 @@ module.exports.navigate = function(venueId, referenceId){
 		var errorCallback = successCallback;
 		cordova.exec(successCallback, errorCallback, "VanillaNav", "navigate", [venueId, referenceId]);
 	} else if(device.platform === 'iOS') {
-		var scheme = "vanillanav://navigateTo?venueId=" + venueId + "&referenceId=" + referenceId;
+		var scheme = 'vanillanav://navigateTo?venueId=' + venueId + '&referenceId=' + referenceId;
 
 		appAvailability.check(
-    		scheme,       // URI Scheme or Package Name
+    		'vanillanav://',       // URI Scheme or Package Name
     		function() {  // Success callback
     			console.log(scheme + ' is available :)');
-    			//window.location=scheme;
     			window.open(scheme, '_system');
     		},
 		    function() {  // Error callback
 		    	console.log(scheme + ' is not available :(');
-		    	//window.location="https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8";
-		    	window.open('https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8', '_system');
+		    	window.open('itms-apps://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8', '_system');
 		    }
 		);
 	}
-
-}
